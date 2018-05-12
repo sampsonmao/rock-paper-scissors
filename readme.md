670dc2fc69a6d9960a71b25628868691bd8473a6
sampsonmao <sampson.mao@gmail.com>
2018-05-12 11:40:09 -0700

    Updated pictures, fixed objects in oneGame function

---
+++ readme.md
@@ -0,0 +1,3 @@
+### Creating a webpage with the rock paper scissors game
+
+Linking HTML, CSS, and Javascript into a webpage where a user can play rock paper scissors with a click of a button. 
--- script.js
+++ script.js
@@ -18,22 +18,22 @@ function oneGame(playerSelection){
         return {player: playerSelection, computer: computerSelection, result: 'lose'};
     }
     else if (playerSelection=='rock' && computerSelection=='scissors') {
-        return {player: playerSelection, computer: playerSelection, result: 'win'};
+        return {player: playerSelection, computer: computerSelection, result: 'win'};
     }    
     else if (playerSelection=='paper' && computerSelection=='rock') {
-        return {player: playerSelection, computer: playerSelection, result: 'win'};
+        return {player: playerSelection, computer: computerSelection, result: 'win'};
     }
     else if (playerSelection=='paper' && computerSelection=='scissors') {
-        return {player: playerSelection, computer: playerSelection, result: 'lose'};
+        return {player: playerSelection, computer: computerSelection, result: 'lose'};
     }    
     else if (playerSelection=='scissors' && computerSelection=='paper') {
-        return {player: playerSelection, computer: playerSelection, result: 'win'}
+        return {player: playerSelection, computer: computerSelection, result: 'win'}
     }
     else if (playerSelection=='scissors' && computerSelection=='rock') {
-        return {player: playerSelection, computer: playerSelection, result: 'lose'};
+        return {player: playerSelection, computer: computerSelection, result: 'lose'};
     }
     else if ((playerSelection=='rock' && computerSelection=='rock') || (playerSelection=='paper' && computerSelection=='paper') || (playerSelection=='scissors' && computerSelection=='scissors')) {
-        return {player: playerSelection, computer: playerSelection, result: 'draw'}
+        return {player: playerSelection, computer: computerSelection, result: 'draw'}
     }
     else {
         return "Invalid input. Please enter rock, paper, or scissors."
@@ -48,20 +48,28 @@ let numLose = 0;
 let numDraw = 0
 
 function playGame(e) {
-    gameResult = oneGame(e.target.id).result
-    console.log(gameResult)
+    gameObject = oneGame(e.target.id)
 
-    if (gameResult == 'win') {
+    if (gameObject.result == 'win') {
         numWin += 1;
         document.getElementById("playerScore").textContent = numWin;
+        document.getElementById("playerChoice").textContent = "You chose: " + gameObject.player + "."
+        document.getElementById("computerChoice").textContent = "Computer plays: " + gameObject.computer + "."
+        document.getElementById("result").textContent = "Win!"
     }
-    else if (gameResult == 'lose') {
+    else if (gameObject.result == 'lose') {
         numLose += 1;
         document.getElementById("computerScore").textContent = numLose;
+        document.getElementById("playerChoice").textContent = "You chose: " + gameObject.player + "."
+        document.getElementById("computerChoice").textContent = "Computer plays: " + gameObject.computer + "."
+        document.getElementById("result").textContent = "Lose."
     }
-    else if (gameResult== 'draw') {
+    else if (gameObject.result == 'draw') {
         numDraw += 1;
         document.getElementById("drawScore").textContent = numDraw;
+        document.getElementById("playerChoice").textContent = "You chose: " + gameObject.player + "."
+        document.getElementById("computerChoice").textContent = "Computer plays: " + gameObject.computer + "."
+        document.getElementById("result").textContent = "Draw!"
     }
     else {
         return
